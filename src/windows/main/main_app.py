82d7c8a53ed8e312196358b63a16404cb936bea0
boywaste <spartacrafter@gmail.com>
2024-07-16 00:28:13 -0700

    added stuff

--- src/windows/main/main_app.py
+++ src/windows/main/main_app.py
@@ -1,6 +1,7 @@
 import json
 import sys
 from tkinter import *
+from tkinter import ttk
 
 from utils.not_windows import NotWindows
 from windows.window import Window
@@ -30,7 +31,7 @@ class MainApp(Window):
     """Main windows of the application"""
 
     def __init__(self):
-        super().__init__("PyMacroRecord", 350, 200)
+        super().__init__("PyMacroRecord", 300, 200)
         self.attributes("-topmost", 1)
         if platform == "win32":
             self.iconbitmap(resource_path(path.join("assets", "logo.ico")))
@@ -58,6 +59,38 @@ class MainApp(Window):
         self.hotkeyManager = HotkeysManager(self)
 
         # Main Buttons (Start record, stop record, start playback, stop playback)
+        top_frame = Frame(self)
+        top_frame.pack(fill=BOTH, expand=True, padx=16)
+        top_frame.columnconfigure(0, weight=1)
+        top_frame.columnconfigure(1, weight=3)
+
+        Label(top_frame, text = 'Speed:').grid(column=0, row=0, padx=8)
+        self.speed_value = DoubleVar()
+        self.speed_value.set(self.settings.get_config()["Playback"]["Speed"])
+        Scale(
+            top_frame,
+            from_=1.0,
+            to=1000,
+            orient='horizontal',
+            length=150,
+            showvalue=1,
+            resolution=0.01,
+            command=self.speed_changed,
+            variable=self.speed_value
+        ).grid(column=1, row=0, padx=8)
+
+        Label(top_frame, text = 'Repeat:').grid(column=0, row=1, padx=8)
+        self.repeatSpinbox = ttk.Spinbox(
+            top_frame,
+            from_=1,
+            to=999999999,
+            command=self.repeat_changed
+        )
+        self.repeatSpinbox.grid(column=1, row=1, padx=8)
+        self.repeatSpinbox.set(self.settings.get_config()["Playback"]["Repeat"]["Times"])
+
+        bot_frame = Frame(self)
+        bot_frame.pack(fill=BOTH, expand=True, padx=16)
 
         # Play Button
         self.playImg = PhotoImage(file=resource_path(path.join("assets", "button", "play.png")))
@@ -67,17 +100,17 @@ class MainApp(Window):
             with open(sys.argv[1], 'r') as record:
                 loaded_content = load(record)
             self.macro.import_record(loaded_content)
-            self.playBtn = Button(self, image=self.playImg, command=self.macro.start_playback)
+            self.playBtn = Button(bot_frame, image=self.playImg, command=self.macro.start_playback)
             self.macro_recorded = True
             self.macro_saved = True
         else:
-            self.playBtn = Button(self, image=self.playImg, state=DISABLED)
-        self.playBtn.pack(side=LEFT, padx=50)
+            self.playBtn = Button(bot_frame, image=self.playImg, state=DISABLED)
+        self.playBtn.pack(side=LEFT, padx=8)
 
         # Record Button
         self.recordImg = PhotoImage(file=resource_path(path.join("assets", "button", "record.png")))
-        self.recordBtn = Button(self, image=self.recordImg, command=self.macro.start_record)
-        self.recordBtn.pack(side=RIGHT, padx=50)
+        self.recordBtn = Button(bot_frame, image=self.recordImg, command=self.macro.start_record)
+        self.recordBtn.pack(side=RIGHT, padx=8)
 
         # Stop Button
         self.stopImg = PhotoImage(file=resource_path(path.join("assets", "button", "stop.png")))
@@ -89,7 +122,8 @@ class MainApp(Window):
         self.bind('<Control-l>', record_management.load_macro)
         self.bind('<Control-n>', record_management.new_macro)
 
-        self.protocol("WM_DELETE_WINDOW", self.quit_software)
+        self.protocol("WM_DELETE_WINDOW", self.withdraw)
+
         if platform.lower() != "darwin":
             Thread(target=self.systemTray).start()
 
@@ -103,13 +137,21 @@ class MainApp(Window):
                 if time() > self.settings.get_config()["Others"]["Remind_new_ver_at"]:
                     NewVerAvailable(self, self.version.new_version)
 
+        self.withdraw()
         self.mainloop()
+
+    def speed_changed(self, event):
+        self.settings.change_settings("Playback", "Speed", None, self.speed_value.get())
+
+    def repeat_changed(self):
+        self.settings.change_settings("Playback", "Repeat", "Times", self.repeatSpinbox.get())
 
     def systemTray(self):
         """Just to show little icon on system tray"""
         image = Image.open(resource_path(path.join("assets", "logo.ico")))
         menu = (
             MenuItem('Show', action=self.deiconify, default=True),
+            MenuItem('Quit', action=self.force_quit_window, default=True),
         )
         self.icon = Icon("name", image, "PyMacroRecord", menu)
         self.icon.run()
@@ -124,8 +166,12 @@ class MainApp(Window):
                 return False
         return True
 
-    def quit_software(self, force=False):
+    def force_quit_window(self):
+        self.quit_window(True)
+
+    def quit_window(self, force=False):
         if not self.macro_saved and self.macro_recorded and not force:
+            self.deiconify()
             wantToSave = confirm_save(self)
             if wantToSave:
                 RecordFileManagement(self, self.menu).save_macro()
@@ -133,6 +179,6 @@ class MainApp(Window):
                 return
         if platform.lower() != "darwin":
             self.icon.stop()
-        if platform.lower() == "linux":
-            self.destroy()
+        #if platform.lower() == "linux":
+        #    self.destroy()
         self.quit()
